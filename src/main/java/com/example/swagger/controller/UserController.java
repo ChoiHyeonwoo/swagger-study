7472a5d41fd898df5814ef7c85946d96f6532646
hyeonwoo <escape0619@gmail.com>
2021-03-31 14:08:37 +0900

    @Tag(name = "UserController", description = "사용자 API") 해당 파일에 설명 추가하는 annotation

--- src/main/java/com/example/swagger/controller/UserController.java
+++ src/main/java/com/example/swagger/controller/UserController.java
@@ -7,17 +7,19 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "UserController", description = "사용자 API")
 @RestController
 @RequiredArgsConstructor
 public class UserController {
 
     private final UserService userService;
 
-    @GetMapping("/user")
     @Operation(summary = "사용자의 상세정보를 리턴합니다.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "사용자 정보를 리턴합니다.",
@@ -27,7 +29,8 @@ public class UserController {
                     content = @Content),
             @ApiResponse(responseCode = "404", description = "없는 사용자 입니다.",
                     content = @Content) })
-    public UserDTO getUserData(){
+    @GetMapping("/user/{id}")
+    public UserDTO getUserData(@PathVariable Long id){
         return userService.getUserData();
     }
 }
